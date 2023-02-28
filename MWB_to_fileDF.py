cca044778ed231430e54cb2c1afde418d52ef945
Mingxun Wang <mwang87@gmail.com>
2023-02-28 09:53:17 -0800

    updating MWB for all datasets

--- MWB_to_fileDF.py
+++ MWB_to_fileDF.py
@@ -12,6 +12,8 @@ def _get_metabolomicsworkbench_filepaths(study_id):
         mw_file_list = requests.get(dataset_list_url).json()
         workbench_df = pd.DataFrame(mw_file_list)
         workbench_df = workbench_df['FILENAME']
+
+        workbench_df['STUDY_ID'] = study_id
     except:
         workbench_df = pd.DataFrame()
 
@@ -21,14 +23,38 @@ def _get_metabolomicsworkbench_filepaths(study_id):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Give an MWB study ID and get a dataframe of file paths present in the study.')
-    parser.add_argument("--study_id", "-mwb_id", type=str, help='An MWB study ID such as "ST002050"', required=True)
+    parser.add_argument("--study_id", "-mwb_id", type=str, help='An MWB study ID such as "ST002050", ALL for every study', required=True)
     parser.add_argument("--output_path", type=str, help='The path where the output file should be written. Default is the current working directory.',
                         default=os.getcwd())
 
     args = parser.parse_args()
 
-    result = _get_metabolomicsworkbench_filepaths(study_id=args.study_id)
+    if args.study_id == "ALL":
+        # Getting all files
+        url = "https://www.metabolomicsworkbench.org/rest/study/study_id/ST/available"
+        studies_dict = requests.get(url).json()
+
+        study_list = []
+        for key in studies_dict.keys():
+            study_dict = studies_dict[key]
+            study_list.append(study_dict['study_id'])
+
+        study_list = list(set(study_list))
+
+        all_results_list = []
+        for study_id in study_list:
+            print("Downloading", study_id)
+
+            temp_result_df = _get_metabolomicsworkbench_filepaths(study_id=study_id)
+
+            all_results_list.append(temp_result_df)
+
+        result_df = pd.concat(all_results_list, axis=0)
+
+    else:
+        result_df = _get_metabolomicsworkbench_filepaths(study_id=args.study_id)
+    
     output_file = os.path.join(args.output_path, f"{args.study_id}.csv")
-    result.to_csv(output_file, index=False)
+    result_df.to_csv(output_file, index=False)
 
     print(f"Output written to {output_file}")
